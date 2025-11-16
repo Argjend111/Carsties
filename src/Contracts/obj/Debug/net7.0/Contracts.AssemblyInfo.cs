29486f3747b3a8b1ea0b848637f72b9c5e661f4c
Your Name <your-email@example.com>
2025-11-16 23:09:27 +0100

    changes

--- src/Contracts/obj/Debug/net7.0/Contracts.AssemblyInfo.cs
+++ src/Contracts/obj/Debug/net7.0/Contracts.AssemblyInfo.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16c6a88c8b58034da19fc4f3ea86086e67e0cb17")]
-[assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
-[assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
+[assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 
