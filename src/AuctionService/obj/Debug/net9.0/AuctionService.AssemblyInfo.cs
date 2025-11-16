b59247a97aa5502a701ae3a5958e4d9c9d70c96b
Your Name <your-email@example.com>
2025-11-16 21:20:33 +0100

    changes

--- src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ebfa90c2dada0d05afe0a8b47babd9fc6ad3753")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
