88f21169b2e23f0dc58d828dc604209bffcd80f1
Your Name <your-email@example.com>
2025-11-17 18:24:32 +0100

    changes

---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+
+           
+    }
+}       
