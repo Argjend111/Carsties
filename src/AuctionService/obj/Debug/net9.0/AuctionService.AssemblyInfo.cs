00e55116ac8e9e0f3e7143fab4924b5d4ddb3872
Argjend Bytyçi <argjendd220@gmail.com>
2025-12-07 15:56:37 +0100

    changes

--- src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29486f3747b3a8b1ea0b848637f72b9c5e661f4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aab74d11a4a06cbcd8223533b5d8484098f2b9e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
