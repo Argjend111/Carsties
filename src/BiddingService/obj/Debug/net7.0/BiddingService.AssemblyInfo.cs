63d7024d168a8ed4aabf494b53af1265a4ff1f40
Argjend Bytyçi <argjendd220@gmail.com>
2025-12-08 18:10:34 +0100

    changes for docker

--- src/BiddingService/obj/Debug/net7.0/BiddingService.AssemblyInfo.cs
+++ src/BiddingService/obj/Debug/net7.0/BiddingService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ea9ffb4fc4ac4a458f51de1cb2509503ca38cb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+857cf69ed2b6b89b79ea333360d0f46ec96cc1bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
