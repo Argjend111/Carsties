7f1c604277a2e50760b3bfdbbc767c95f7fc91c3
Argjend Bytyçi <argjendd220@gmail.com>
2026-01-10 01:08:54 +0100

    cf

--- src/Contracts/obj/Debug/net7.0/Contracts.AssemblyInfo.cs
+++ src/Contracts/obj/Debug/net7.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d99631528e6051688a10685c3a2b7db80c9708b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8f4986414f665fc5633f27db5fb92448394ca0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
