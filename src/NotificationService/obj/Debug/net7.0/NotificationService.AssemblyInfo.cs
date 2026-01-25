d2a4ee1ebfb9c4e285f7acfc631bd88b670c594b
Argjend Bytyçi <argjendd220@gmail.com>
2026-01-25 15:21:17 +0100

    FIXED CHANGES

--- src/NotificationService/obj/Debug/net7.0/NotificationService.AssemblyInfo.cs
+++ src/NotificationService/obj/Debug/net7.0/NotificationService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae87a64ad63ad2faab5cf5896c7f2bfe054c276")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f6c0ccbe27af92b972ef2922853794ef6573db")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/AuctionService.IntegrationTests/obj/Debug/net7.0/AuctionService.IntegrationTests.AssemblyInfo.cs
+++ tests/AuctionService.IntegrationTests/obj/Debug/net7.0/AuctionService.IntegrationTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService.IntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae87a64ad63ad2faab5cf5896c7f2bfe054c276")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f6c0ccbe27af92b972ef2922853794ef6573db")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService.IntegrationTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService.IntegrationTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
