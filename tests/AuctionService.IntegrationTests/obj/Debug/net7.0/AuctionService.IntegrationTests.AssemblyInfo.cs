6d7cb97d268115ad639b5d9f7fea90d8e6035391
Argjend Bytyçi <argjendd220@gmail.com>
2026-01-26 20:19:24 +0100

    dd

--- tests/AuctionService.IntegrationTests/obj/Debug/net7.0/AuctionService.IntegrationTests.AssemblyInfo.cs
+++ tests/AuctionService.IntegrationTests/obj/Debug/net7.0/AuctionService.IntegrationTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService.IntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a4ee1ebfb9c4e285f7acfc631bd88b670c594b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d350ee93db9f9c9c5ddd43b96d2c66c0111c87a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService.IntegrationTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService.IntegrationTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
