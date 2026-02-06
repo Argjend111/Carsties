827aa95845699773589573c058bfab2af2bff115
Argjend Bytyçi <argjendd220@gmail.com>
2026-02-06 15:09:05 +0100

    architecture

--- src/GatewayService/obj/Debug/net7.0/GatewayService.AssemblyInfo.cs
+++ src/GatewayService/obj/Debug/net7.0/GatewayService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GatewayService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d71ff14f6411e0652db66205a6d58aea2058b46f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2789eaa230c97577190c56d1188f8ffc303df1db")]
 [assembly: System.Reflection.AssemblyProductAttribute("GatewayService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GatewayService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
