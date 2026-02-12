8f118c6829e82c16c375e2ea4473f267ea2c750d
Argjend Bytyçi <argjendd220@gmail.com>
2026-02-12 19:47:10 +0100

    CC

--- src/BiddingService/obj/Debug/net7.0/BiddingService.AssemblyInfo.cs
+++ src/BiddingService/obj/Debug/net7.0/BiddingService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f827a778b838ce95e65d8ce96979e63685aa78d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d9ae94308767a9732e7d28215b6767e494fd6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/AuctionService.UnitTests/obj/Debug/net7.0/AuctionService.UnitTests.AssemblyInfo.cs
+++ tests/AuctionService.UnitTests/obj/Debug/net7.0/AuctionService.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f827a778b838ce95e65d8ce96979e63685aa78d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d9ae94308767a9732e7d28215b6767e494fd6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
