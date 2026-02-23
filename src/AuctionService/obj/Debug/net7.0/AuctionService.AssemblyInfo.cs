acde372820c080fd3e0f42ca558c0dd3d5dd6b88
Argjend Bytyçi <argjendd220@gmail.com>
2026-02-23 18:45:54 +0100

    filter

--- src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a89cdadb193354e75e6a5e0e9ee2e9941c6b9d36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63a997e5756e05373c891ec14c8a5502c2525291")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
