64199e433721e22d0cd06803232d09d01326e3ac
Argjend Bytyçi <argjendd220@gmail.com>
2026-02-26 12:48:47 +0100

    rn-n

--- src/SearchService/obj/Debug/net7.0/SearchService.AssemblyInfo.cs
+++ src/SearchService/obj/Debug/net7.0/SearchService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a657ba2cdac198fa929ae7bfe4067789fadcf9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266378bbb913d76d55fe867624c571d88c730faf")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
